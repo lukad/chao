961e7ddd4a77e71eaafc6a8e804cdc07a02827b5
Luka Dornhecker <luka.dornhecker@gmail.com>
2018-08-05 09:30:37 +0200

    implement proper Debug for Expr

--- libchao/src/expr.rs
+++ libchao/src/expr.rs
@@ -1,7 +1,9 @@
+use itertools::Itertools;
+
 use env::Env;
 use std::fmt;
 
-#[derive(Debug, Clone)]
+#[derive(Clone)]
 pub enum Expr {
     Nil,
     Bool(bool),
@@ -12,6 +14,22 @@ pub enum Expr {
     Fun(Function, Arguments),
     Special(Function, Arguments),
     List(Vec<Expr>),
+}
+
+impl fmt::Debug for Expr {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Expr::Nil => write!(f, "nil"),
+            Expr::Bool(x) => write!(f, "{:?}", x),
+            Expr::Int(x) => write!(f, "{:?}", x),
+            Expr::Float(x) => write!(f, "{:?}", x),
+            Expr::Str(x) => write!(f, "\"{}\"", x),
+            Expr::Symbol(x) => write!(f, "{}", x),
+            Expr::Fun(_, _) => write!(f, "<function>"),
+            Expr::Special(_, _) => write!(f, "<special>"),
+            Expr::List(xs) => write!(f, "({})", xs.iter().map(|x| format!("{:?}", x)).join(" ")),
+        }
+    }
 }
 
 pub struct Function(pub fn(&mut Env) -> Expr);
