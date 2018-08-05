2322a238c30db537ef7160f3d7e7090f60cd06e6
Luka Dornhecker <luka.dornhecker@gmail.com>
2018-08-05 14:26:31 +0200

    implement lambda

--- libchao/src/env.rs
+++ libchao/src/env.rs
@@ -23,20 +23,42 @@ impl Env {
     }
 
     fn initialize(&mut self) {
-        self.insert("+".to_string(), Fun(Function(add), Arguments::Variadic));
-        self.insert("-".to_string(), Fun(Function(sub), Arguments::Variadic));
-        self.insert("*".to_string(), Fun(Function(mul), Arguments::Variadic));
-        self.insert("/".to_string(), Fun(Function(div), Arguments::Variadic));
-        self.insert("=".to_string(), Fun(Function(eq), Arguments::Variadic));
+        self.insert(
+            "+".to_string(),
+            Fun(Function::Builtin(add), Arguments::Variadic),
+        );
+        self.insert(
+            "-".to_string(),
+            Fun(Function::Builtin(sub), Arguments::Variadic),
+        );
+        self.insert(
+            "*".to_string(),
+            Fun(Function::Builtin(mul), Arguments::Variadic),
+        );
+        self.insert(
+            "/".to_string(),
+            Fun(Function::Builtin(div), Arguments::Variadic),
+        );
+        self.insert(
+            "=".to_string(),
+            Fun(Function::Builtin(eq), Arguments::Variadic),
+        );
         self.insert(
             "if".to_string(),
             Special(
-                Function(iff),
+                Function::Builtin(iff),
                 Arguments::Fixed(vec![
                     "cond".to_string(),
                     "expr1".to_string(),
                     "expr2".to_string(),
                 ]),
+            ),
+        );
+        self.insert(
+            "lambda".to_string(),
+            Special(
+                Function::Builtin(lambda),
+                Arguments::Fixed(vec!["args".to_string(), "body".to_string()]),
             ),
         );
     }
@@ -46,8 +68,8 @@ impl Env {
             Int(x) => Int(*x),
             Bool(x) => Bool(*x),
             Str(x) => Str(x.clone()),
-            Symbol(x) => Symbol(x.clone()),
-            Fun(f, args) => Fun(*f, args.clone()),
+            Symbol(x) => self.get(x.clone()).unwrap(),
+            Fun(f, args) => Fun(f.clone(), args.clone()),
             List(list) => self.eval_list(list),
             _ => Nil,
         }
@@ -79,28 +101,35 @@ impl Env {
 
     fn eval_list(&mut self, list: &Vec<Expr>) -> Expr {
         match &list[..] {
-            [Symbol(s), rest..] => match self.get(s.clone()) {
-                Some(Fun(func, arg_names)) => {
+            [expr, rest..] => match self.eval(expr) {
+                Fun(fun, arg_names) => {
                     let mut evaluated_args: Vec<Expr> = vec![];
                     self.enter();
                     for expr in rest.iter() {
                         evaluated_args.push(self.eval(expr));
                     }
-                    self.bind_args(evaluated_args, arg_names);
-                    let result = func.0(self);
+                    self.bind_args(evaluated_args, arg_names.clone());
+                    let result = self.eval_fun(fun.clone());
                     self.exit();
                     result
                 }
-                Some(Special(func, arg_names)) => {
+                Special(fun, arg_names) => {
                     self.enter();
-                    self.bind_args(rest.to_vec(), arg_names);
-                    let result = func.0(self);
+                    self.bind_args(rest.to_vec(), arg_names.clone());
+                    let result = self.eval_fun(fun.clone());
                     self.exit();
                     result
                 }
-                other => panic!("Can't apply {:?}: {:?}", s, other),
+                other => panic!("Can't apply {:?}", other),
             },
-            _ => panic!("Can't apply {:?}", list),
+            [] => Nil,
+        }
+    }
+
+    fn eval_fun(&mut self, fun: Function) -> Expr {
+        match fun {
+            Function::Builtin(builtin) => builtin(self),
+            Function::Dynamic(expr) => self.eval(&expr),
         }
     }
 
@@ -183,5 +212,23 @@ fn eq(env: &mut Env) -> Expr {
         }
     } else {
         panic!("eq requires a at least one argument")
+    }
+}
+
+fn lambda(env: &mut Env) -> Expr {
+    let body = env.get("body".to_string()).unwrap();
+    match env.get("args".to_string()).unwrap() {
+        List(args) => {
+            let arguments = Arguments::Fixed(
+                args.iter()
+                    .map(|arg| match arg {
+                        Symbol(s) => s.clone(),
+                        _ => panic!("lambda arguments must be symbols"),
+                    })
+                    .collect::<Vec<_>>(),
+            );
+            Fun(Function::Dynamic(Box::new(body)), arguments)
+        }
+        _ => panic!("This should not happen"),
     }
 }
